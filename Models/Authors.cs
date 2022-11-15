3458438226308c2d278eb7354b8bd2e26678dd49
Maria Volintiru <maria.volintiru@yahoo.com>
2022-11-15 18:59:13 +0200

    lab3lab2, migrare si dropdownlist

---
+++ Models/Authors.cs
@@ -0,0 +1,12 @@
+﻿namespace Volintiru_Maria_Lab2.Models
+{
+    public class Author
+    {
+            public int ID { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public ICollection<Book>? Book { get; set; }
+        }
+    
+}
