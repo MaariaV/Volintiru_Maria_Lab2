551c8ce2880b68017a6b1794c86b473a85d5b63d
Maria Volintiru <maria.volintiru@yahoo.com>
2022-11-18 14:46:17 +0200

    lab2 nou

---
+++ Models/LibraryViewModels/PublisherIndexData.cs
@@ -0,0 +1,12 @@
+﻿namespace Volintiru_Maria_Lab2.Models.LibraryViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher>? Publishers { get; set; }
+        public IEnumerable<Book>? Books { get; set; }
+        public IEnumerable<Order>? Orders
+        {
+            get; set;
+        }
+    }
+}
---
+++ Models/PublishedBook.cs
@@ -0,0 +1,11 @@
+﻿namespace Volintiru_Maria_Lab2.Models
+{
+    public class PublishedBook
+    {
+        public int PublisherID { get; set; }
+        public int BookID { get; set; }
+        public Publisher? Publisher { get; set; }
+        public Book? Book { get; set; }
+
+    }
+}
